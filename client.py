5a968edaf83e3b46c895f1f282e864c1cc9b13e1
Carlos Requena <crequena@ulb.ac.be>
2018-11-06 17:17:24 +0100

    bootstrap project, simple service for tracker,
    function signatures for the client
    add README

---
+++ client.py
@@ -0,0 +1,34 @@
+import requests
+
+
+def select_nodes(node_pool):
+    """Selects 3 public nodes from the available pool
+
+    """
+    pass
+
+
+def conn():
+    """Connect to the torrent network, uploading the list of files this
+    client has
+
+    """
+    pass
+
+
+def request_file(file_name):
+    """Asks for the file to the tracker
+
+    """
+    pass
+
+
+def client_loop():
+    """This function makes the client interactive and puts the terminal in
+    a read-eval loop where the input + newline is considered the file
+    this client is requesting to the tracker/torrent network
+
+    """
+    # read eval loop from stdin
+    # send request
+    pass
