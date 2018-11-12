2d577f561eb1b91efa605359ea80b6f463500426
Théo Verhelst <tverhels@ulb.ac.be>
2018-11-12 23:04:53 +0100

    Add file documentation to crypto.py

--- crypto.py
+++ crypto.py
@@ -1,6 +1,31 @@
+"""Contains the encryption primitives, namely the Advanced Encryption Standard
+(AES). Only the function 'encrypt' is meant to be used by the outside, all the
+others are for implementation purposes and are private.
+
+This file works with the Python type 'bytes', which is basically an immutable
+list of byte values. The message has to be of type bytes, and the key as well,
+and the key has to be 16 bytes long.
+"""
 
 n_rounds = 10 # Defined in the standard of AES for 128 bits key
 block_size = 16 # Number of bytes in one message or one key
+
+def encrypt(plain_text, key):
+    assert type(plain_text) == bytes, "plain_text must be of type bytes"
+    assert type(key) == bytes, "key must be of type bytes"
+    assert len(key) == block_size, "key must be 128 bits"
+
+    # Add padding to the plain text in order to have blocks of 128 bits
+    plain_text = plain_text + bytes(len(plain_text) % block_size)
+    cypher_text = bytes()
+
+    for i in range(len(plain_text) // message_size):
+        # TODO We should use another mode than using always the same cypher
+        key_schedule = expand_key(key)
+        plain_text_block = plain_text[i * block_size : (i + 1) * block_size]
+        cypher_text += aes_encrypt_block(plain_text_block, key_schedule)
+
+    return cypher_text
 
 def aes_encrypt_block(plain_text, key_schedule):
     """Encrypts a message using AES-128.
@@ -48,20 +73,3 @@ def expand_key(key):
     assert type(key) == bytes, "key must be of type bytes"
     assert len(key) == 16, "key must be 128 bits"
     return key * (n_rounds + 1) # TODO implement the real expand key alg
-
-def encrypt(plain_text, key):
-    assert type(plain_text) == bytes, "plain_text must be of type bytes"
-    assert type(key) == bytes, "key must be of type bytes"
-    assert len(key) == block_size, "key must be 128 bits"
-
-    # Add padding to the plain text in order to have blocks of 128 bits
-    plain_text = plain_text + bytes(len(plain_text) % block_size)
-    cypher_text = bytes()
-
-    for i in range(len(plain_text) // message_size):
-        # TODO We should use another mode than using always the same cypher
-        key_schedule = expand_key(key)
-        plain_text_block = plain_text[i * block_size : (i + 1) * block_size]
-        cypher_text += aes_encrypt_block(plain_text_block, key_schedule)
-
-    return cypher_text
