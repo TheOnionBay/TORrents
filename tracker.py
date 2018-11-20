b94bc567e244d3896dc584d208ac183b78fa6289
Carlos Requena López <crequena@ulb.ac.be>
2018-11-20 13:44:28 +0100

    continue function signatures and structure

--- tracker.py
+++ tracker.py
@@ -8,5 +8,20 @@ def index():
     # Process list of files a client has
     return jsonify(files)
 
+@app.route("/announce", methods=['POST'])
+def announce():
+    """Gets list of files from some client and updates table accordingly.
+
+    """
+    pass
+
+@app.route("/request", methods=['POST'])
+def request():
+    """Gets the name of the file wanted by some peer and check
+    availability
+
+    """
+    pass
+
 
 app.run()
