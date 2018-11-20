52faf9a4afff30a5c44ddf564bc0630675fcfd7a
Théo Verhelst <tverhels@ulb.ac.be>
2018-11-20 17:49:11 +0100

    Make RSA functions accept bytes rather than directly int
    The fact that RSA uses int is an implementation detail. Now the user
    just have to send a bytes object, juste like for AES encryption. The
    maximum size of a message is defined by rsa.max_message_length

--- crypto/test_rsa.py
+++ crypto/test_rsa.py
@@ -1,14 +1,13 @@
 import unittest
 import rsa
-from random import randrange
+from random_bytes import generate_bytes
 
 class TestRsa(unittest.TestCase):
     def test_encrypt_then_decrypt(self):
-        pub, priv = rsa.generate_rsa(1024)
+        pub, priv = rsa.generate_rsa()
         number_of_tests = 128
         # Generate a bunch of random messages to encrypt and then decrypt.
-        # These messages have to be smaller than the modulus of the key, n.
-        test_values = [randrange(0, pub.n) for _ in range(number_of_tests)]
+        test_values = [generate_bytes(rsa.max_message_length) for _ in range(number_of_tests)]
 
         for test_value in test_values:
             cipher_text = rsa.rsa_encrypt(test_value, pub)
