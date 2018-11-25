260ffa8ec4af267e56bdf4a83ca6317ede4506f1
Carlos Requena López <crequena@ulb.ac.be>
2018-11-25 18:29:16 +0100

    tracker user interface. closes #9

--- tracker.py
+++ tracker.py
@@ -1,12 +1,38 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
 from flask import jsonify
-app = Flask(__name__)
+from common.network_info import *
 
-files = ["titanic", "Harry Potter", "Resident Evil", "MatLab"]
-@app.route("/")
+app = Flask(__name__, template_folder=os.path.abspath('tracker/templates'))
+
+files = {"movie1": "cid1",
+         "movie2": "cid2",
+         "movie3": "cid3"}
+
+peers = {"cid1": "IP1",
+         "cid2": "IP2"}
+
+
+def send_filestore(cid):
+    """Clients need the list of all files available in the network
+    (statement of the project). Therefore this function has to be
+    called at some point to share that list of files with the clients.
+
+    """
+    # send files to cid.
+    pass
+
+
+@app.route("/", methods=['POST'])
+def index2():
+    return "is this route necessary ?"
+
+@app.route("/", methods=['GET'])
 def index():
     # Process list of files a client has
-    return jsonify(files)
+    return render_template("index.html",
+                           data={"file_list": list(files.keys()),
+                                 "peers": peers})
 
 @app.route("/announce", methods=['POST'])
 def announce():
