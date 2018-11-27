c521d03455ae4440b6bfc6e0279a0d75779381d5
Carlos Requena López <crequena@ulb.ac.be>
2018-11-27 14:36:29 +0100

    [bugfix] fixes #18

--- tracker.py
+++ tracker.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, jsonify, request, abort
 from common.network_info import cid_size
 from crypto.random_bytes import generate_bytes
 
+
 class Tracker(Flask):
     def __init__(self):
         super().__init__(__name__, template_folder=os.path.abspath('tracker/templates'))
@@ -11,11 +12,11 @@ class Tracker(Flask):
         self.add_url_rule("/", "main_handler", self.main_handler, methods=["POST"])
 
         self.files = {"movie1": "cid1",
-                 "movie2": "cid2",
-                 "movie3": "cid3"}
+                      "movie2": "cid2",
+                      "movie3": "cid3"}
 
         self.peers = {"cid1": "IP1",
-                 "cid2": "IP2"}
+                      "cid2": "IP2"}
 
         self.fsid_counter = 0
 
@@ -58,7 +59,8 @@ class Tracker(Flask):
                 "files": list(self.files.keys())
             }
         }
-        requests.post(ip, data=response)
+        url = "http://" + ip
+        requests.post(url, data=response)
         return "ok"
 
     def handle_file_request(self, request_client_cid, file):
@@ -88,7 +90,7 @@ class Tracker(Flask):
             "FSID": fsid
         }
         # Send on the control channel of the node
-        requests.post(owning_client_IP + "/control/", data=make_bridge_message)
+        requests.post("http://" + owning_client_ip + "/control/", data=make_bridge_message)
 
         # Send a message to the node at the end of the bridge
         receive_bridge_message = {
@@ -97,18 +99,18 @@ class Tracker(Flask):
             "bridge_CID": bridge_cid
         }
         # Send on the control channel of the node
-        requests.post(request_client_ip + "/control/", data=receive_bridge_message)
+        requests.post("http://" + request_client_ip + "/control/", data=receive_bridge_message)
 
         # Send a message to the client, asking he/she to send the file
         request_message = {
-            "CID": owning_client_CID,
+            "CID": owning_client_cid,
             "payload": {
                 "type": "request",
                 "file": file,
                 "FSID": fsid
             }
         }
-        requests.post(request_client_ip, data=request_message)
+        requests.post("http://" + request_client_ip, data=request_message)
         return "ok"
 
 tracker = Tracker()
